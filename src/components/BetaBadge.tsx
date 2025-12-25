c0e63cdaba2e039189110da2404c575350afaf13
pieterharmtillema-dev <pieterharmtillema@gmail.com>
2025-12-25 17:22:56 +0100

    Add BetaBadge component for beta indication

---
+++ src/components/BetaBadge.tsx
@@ -0,0 +1,9 @@
+export default function BetaBadge() {
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+      <div className="rounded-full bg-yellow-400/90 px-4 py-2 text-xs font-bold text-black shadow-lg">
+        BETA
+      </div>
+    </div>
+  );
+}
